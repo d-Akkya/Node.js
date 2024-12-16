65ce3379715eaff8d5e6ab4b5dd744c777dfeaf2
d-Akkya <akhileshbamane26@gmail.com>
2024-12-16 09:25:28 +0530

    dynamic routing

--- index.js
+++ index.js
@@ -8,28 +8,43 @@ const PORT = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
 			// STATIC ROUTING
 
-	if (req.url === "/") {
-		fs.readFile(path.join(__dirname, "public", "index.html"), (err, data) => {
-			if (err) throw err;
-			res.writeHead(200, {"Content-Type": "text/html"});
-			res.end(data)
-		})
-	}
+	// if (req.url === "/") {
+	// 	fs.readFile(path.join(__dirname, "public", "index.html"), (err, data) => {
+	// 		if (err) throw err;
+	// 		res.writeHead(200, {"Content-Type": "text/html"});
+	// 		res.end(data)
+	// 	})
+	// }
 
-	else if (req.url === "/about") {
-		fs.readFile(path.join(__dirname, "public", "about.html"), (err, data) => {
-			if (err) throw err;
-			res.writeHead(200, {"Content-Type": "text/html"});
-			res.end(data)
-		})
-	}
+	// else if (req.url === "/about") {
+	// 	fs.readFile(path.join(__dirname, "public", "about.html"), (err, data) => {
+	// 		if (err) throw err;
+	// 		res.writeHead(200, {"Content-Type": "text/html"});
+	// 		res.end(data)
+	// 	})
+	// }
 
-	else if (req.url === "/contact") {
-		fs.readFile(path.join(__dirname, "public", "contact.html"), (err, data) => {
-			if (err) throw err;
+	// else if (req.url === "/contact") {
+	// 	fs.readFile(path.join(__dirname, "public", "contact.html"), (err, data) => {
+	// 		if (err) throw err;
+	// 		res.writeHead(200, {"Content-Type": "text/html"});
+	// 		res.end(data)
+	// 	})
+	// }
+
+			// DYNAMIC ROUTING
+	let filePath = path.join(__dirname, "public", req.url === "/" ? "index.html" : req.url + ".html")
+
+	fs.readFile(filePath, (err, data) => {
+		if (err) {
+			res.writeHead(404, {"Content-Type": "text/html"});
+			res.end("<h1>404 Not Found!</h1>");
+		}
+		else {
 			res.writeHead(200, {"Content-Type": "text/html"});
 			res.end(data)
-		})
-	}
+		}
+	})
+})
 
 server.listen(PORT, () => console.log(`Server is running successfully on port ${PORT}`))
